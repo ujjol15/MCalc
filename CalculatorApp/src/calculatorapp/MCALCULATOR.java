b092c99a9fcb56983166a13ec82873f1400c26be
ujjol15 <ujjolbd14@gmail.com>
2015-08-22 18:13:16 +0600

    mCalculator
    Ujjol

---
+++ CalculatorApp/src/calculatorapp/MCALCULATOR.java
@@ -0,0 +1,19 @@
+
+
+package calculatorapp;
+
+import javax.swing.JFrame; 
+
+
+public class MCALCULATOR {
+     public static void main(String []args)
+    {
+        CalculatorApp ob=new CalculatorApp();
+       ob.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+ob.setSize( 560, 360 ); 
+ ob.setVisible( true ); 
+    
+    
+    
+    }
+}
